0f3cc8263682df78b1f82a3a083413e77ffdbb62
Alex Lecco <alexlecco24@gmail.com>
2024-03-15 14:25:48 -0300

    update home screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Quizbox Vision </h1>
+        <p> lets play a quiz game </p>
+        <button className='App-button'>start</button>
       </header>
     </div>
   );
