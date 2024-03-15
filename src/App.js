458f18ff096e9667ff54324f79bdaf8243b356ef
Alex Lecco <alexlecco24@gmail.com>
2024-03-15 14:32:08 -0300

    get questions from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react' 
 import './App.css';
 
 function App() {
+  const [ questions, setQuestions ] = useState({})
+
+  useEffect(() => {
+    const url = 'https://opentdb.com/api.php?amount=10'
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setQuestions(data.results))
+  }, [])
+
+  console.log("questions:::::::::::", questions)
+
   return (
     <div className="App">
       <header className="App-header">
